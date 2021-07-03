c5574108966571627833abf52d546015380bb193
Renato Santos <renato_jfs@hotmail.com>
2021-07-03 19:39:48 -0300

    Chapter 2 - Styled Components - Finalizando a listagem de transações

--- src/components/TransactionCard/styles.ts
+++ src/components/TransactionCard/styles.ts
@@ -2,10 +2,16 @@ import styled from 'styled-components/native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { Feather } from '@expo/vector-icons';
 
+interface TransactionProps {
+    type: 'positive' | 'negative';
+    
+}
+
 export const Container = styled.View`
     background-color: ${({ theme }) => theme.colors.shape};
     border-radius: 5px;
     padding: 17px 24px;
+    margin-bottom: 16px;
 `;
 
 export const Title = styled.Text`
@@ -13,9 +19,11 @@ export const Title = styled.Text`
     font-size: ${RFValue(14)}px;
 `;
 
-export const Amount = styled.Text`
+export const Amount = styled.Text<TransactionProps>`
     font-family: ${({ theme }) => theme.fonts.regular};
     font-size: ${RFValue(20)}px;
+    color: ${({ theme, type }) => 
+    type === 'positive' ? theme.colors.sucess : theme.colors.attention};
     margin-top: 2px;
 `;
 
